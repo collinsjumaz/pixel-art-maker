6c93c7ab6f7d89448fb078a2a7ef68ab27abd5cc
collins juma <collinsjumaz42@gmail.com>
2018-06-29 08:29:35 +0300

    javascript functionality

--- designs.js
+++ designs.js
@@ -3,8 +3,49 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
 
-// Your code goes here!
-
+   function makeGrid() {
+	console.log("makeGrid is running!")
+	
+	// Select size input
+	
+	var height, width, rows, canvas, cell;
+	
+	canvas = $('#pixel_canvas');
+	height = $('#input_height').val();
+	width = $('#input_width').val();
+	
+	canvas.children().remove()
+	
+	for (x = 0; x < height; x++) {
+	canvas.append('<tr></tr>');
+	}
+	
+	rows = $('tr');
+	
+	for (i = 0; i < width; i++) {
+	rows.append('<td></td>');
+	} 
+	
+	cell = canvas.find('td');
+	
+	
+	cell.click(function() {
+		
+		console.log("changeColor is running!");
+		var color;
+		color = $("#colorPicker").val();
+		$(this).attr('bgcolor', color);
+	});
+	
 }
+
+
+var submitQuery;
+
+submitQuery = $('input[type="submit"]')
+
+submitQuery.click(function(event) {
+  event.preventDefault();
+  makeGrid();
+});
